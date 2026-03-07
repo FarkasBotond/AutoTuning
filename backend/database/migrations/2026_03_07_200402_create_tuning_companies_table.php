3677938238efbb606fddcc65d2313bd42ffc6aaf
72743314449 <pogabor3@gmail.com>
2026-03-07 22:48:02 +0100

    removed timestamps from tuning_compenies

--- backend/database/migrations/2026_03_07_200402_create_tuning_companies_table.php
+++ backend/database/migrations/2026_03_07_200402_create_tuning_companies_table.php
@@ -16,7 +16,6 @@ return new class extends Migration
             $table->string("name", 50);
             $table->text("description");
             $table->string("website_url");
-            $table->timestamps();
         });
     }
 
