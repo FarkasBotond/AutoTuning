bf3d0dbb8edcfc2849073e5b62d9866f986edf11
72743314449 <pogabor3@gmail.com>
2026-03-07 21:52:08 +0100

    company_car_model migration

---
+++ backend/database/migrations/2026_03_07_200917_create_company_car_model_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('company_car_model', function (Blueprint $table) {
+            $table->foreignId("company_id")->constrained("tuning_companies", "id");
+            $table->foreignId("model_id")->constrained("car_models", "id");
+            $table->primary(["company_id", "model_id"]);
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('company_car_model');
+    }
+};
