d4ae3d15e2e444db66361294efa1180192d4831d
FarkasBotond <fabozo2006@gmail.com>
2026-04-13 16:29:37 +0200

    moved pages to pages/ folder, also created basic landing page layout

--- frontend/src/components/LandingPage.vue
+++
@@ -1,8 +0,0 @@
-<script setup>
-</script>
-
-<template>
-    <Layout>
-        <div class="products">Landing content</div>
-    </Layout>
-</template>
---
+++ frontend/src/pages/LandingPage.vue
@@ -0,0 +1,49 @@
+<script setup>
+</script>
+
+<template>
+    <div class="landing-page">
+        <header>
+            <div class="logo">
+                <img>
+            </div>
+            <div class="search">
+                <nav class="navigation">
+                    <a href="#">Home</a>
+                    <a href="#">About</a>
+                    <a href="#">Contact</a>
+
+                    <input type="text" placeholder="Search...">
+                </nav>
+            </div>
+
+            <div class="user-section">
+                <div class="profile">
+                    <button>Profile</button>
+                </div>
+
+                <div class="cart">
+                    <button>Cart</button>
+                </div>
+            </div>
+
+        </header>
+
+        <main>
+            <div class="menu">
+
+            </div>
+
+            <section class="products">
+
+            </section>
+        </main>
+
+        <footer>
+            <div class="contact">
+                Contact
+            </div>
+            
+        </footer>
+    </div>
+</template>
