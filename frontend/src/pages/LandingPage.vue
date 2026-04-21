170f68bf290b7608335f814b2b2f53e80f4a00f0
FarkasBotond <fabozo2006@gmail.com>
2026-04-21 23:30:48 +0200

    added productcard to landingpage

--- frontend/src/pages/LandingPage.vue
+++ frontend/src/pages/LandingPage.vue
@@ -3,6 +3,30 @@ import SearchBar from '@/components/ui/SearchBar.vue'
 import logo from '@/assets/logo.png'
 import BaseHeadLine from '@/components/layout/BaseHeadLine.vue'
 import SideMenu from '@components/layout/SideMenu.vue';
+import ProductCard from '@components/ProductCard.vue';
+
+const products = [
+    {
+        id: 1,
+        name: 'Állítható sportfutómű Audi A3',
+        brand: 'TA Technix',
+        image: '',
+        badge: 'Újdonság',
+        oldPrice: 126000,
+        price: 103790,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 2,
+        name: 'Sportülés fekete',
+        brand: 'Bride',
+        image: '',
+        badge: 'Akció',
+        oldPrice: 94670,
+        price: 81630,
+        stockText: 'Raktáron'
+    }
+]
 </script>
 
 <template>
@@ -14,9 +38,11 @@ import SideMenu from '@components/layout/SideMenu.vue';
         <main>
             <SideMenu/>
 
-            <section class="products">
-
-            </section>
+            <ProductCard
+            v-for="product in products"
+            :key="product.id"
+            :product="product"
+            />
         </main>
 
         <footer>
@@ -31,9 +57,5 @@ import SideMenu from '@components/layout/SideMenu.vue';
 <route lang="yaml">
 name: landing
 meta:
-<<<<<<< HEAD
   title: Landing
-=======
-  title: Landing Page
->>>>>>> 00e9019 (Vue files (admin login and admin panel/page for changes))
 </route>
