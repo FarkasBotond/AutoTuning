65627192a807b266d392ba542f67ca9082a1aa89
FarkasBotond <fabozo2006@gmail.com>
2026-04-21 23:13:38 +0200

    made a productcard for the cards. No styling

---
+++ frontend/src/components/ProductCard.vue
@@ -0,0 +1,45 @@
+<script setup>
+defineProps({
+    product: {
+        type: Object,
+        required: true
+    }
+})
+</script>
+
+<template>
+    <article>
+        <div>
+            <img
+                :src="product.image"
+                :alt="product.name"
+            >
+
+            <span v-if="product.badge">
+                {{ product.badge }}
+            </span>
+        </div>
+
+        <div>
+            <h3>{{ product.name }}</h3>
+
+            <p>{{ product.brand }}</p>
+
+            <div>
+                <span v-if="product.oldPrice">
+                    {{ product.oldPrice }} Ft
+                </span>
+
+                <span>
+                    {{ product.price }} Ft
+                </span>
+            </div>
+
+            <p>{{ product.stockText }}</p>
+
+            <button type="button">
+                Megnézem
+            </button>
+        </div>
+    </article>
+</template>
