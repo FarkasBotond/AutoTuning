ac8fe43fc9a9601a1481e0f8565767548e65406b
FarkasBotond <fabozo2006@gmail.com>
2026-04-24 13:32:15 +0200

    mockProduct for testing

---
+++ frontend/src/lib/mockProducts.js
@@ -0,0 +1,121 @@
+import allithatofutomuImg from '@/assets/images/allithatofutomu.jpg'
+import belsoledImg from '@/assets/images/belsoled.jpg'
+import diffuserImg from '@/assets/images/diffuser.jpg'
+import downpipeImg from '@/assets/images/downpipe.jpg'
+import fekbetetImg from '@/assets/images/fekbetet.jpg'
+import fekcsoImg from '@/assets/images/fekcso.webp'
+import fektarcsaImg from '@/assets/images/fektarcsa.jpg'
+import hatsodobImg from '@/assets/images/hatsodob.jpg'
+import intercoolerImg from '@/assets/images/intercooler.jpg'
+import kerekdolesImg from '@/assets/images/kerekdoles.jpg'
+import kesztyuImg from '@/assets/images/kesztyu.webp'
+import kipuvegImg from '@/assets/images/kipuveg.webp'
+import kormanyImg from '@/assets/images/kormany.webp'
+import kozepdobImg from '@/assets/images/kozepdob.jpg'
+import ledizzoImg from '@/assets/images/ledizzo.webp'
+import legszuroImg from '@/assets/images/legszuro.jpg'
+import obdImg from '@/assets/images/obd.webp'
+import olajhutoImg from '@/assets/images/olajhuto.jpg'
+import parkszenzorImg from '@/assets/images/parkszenzor.avif'
+import pedalszettImg from '@/assets/images/pedalszett.jpg'
+import rendszamImg from '@/assets/images/rendszam.webp'
+import spoilerImg from '@/assets/images/spoiler.webp'
+import szilikoncsoImg from '@/assets/images/szilikoncso.jpg'
+import tolatokameraImg from '@/assets/images/tolatokamera.avif'
+import turbomeroImg from '@/assets/images/turbomero.jpg'
+import ulesImg from '@/assets/images/ules.webp'
+import valtogombImg from '@/assets/images/valtogomb.png'
+import turboImg from '@/assets/images/turbo.jpg'
+
+export const products = [
+    {
+        id: 1,
+        name: 'Állítható sportfutómű Audi A3',
+        brand: 'TA Technix',
+        image: allithatofutomuImg,
+        badge: 'Újdonság',
+        oldPrice: 126000,
+        price: 103790,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 2,
+        name: 'Sportülés fekete',
+        brand: 'Bride',
+        image: ulesImg,
+        badge: 'Akció',
+        oldPrice: 94670,
+        price: 81630,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 3,
+        name: 'Garrett turbó',
+        brand: 'Garrett',
+        image: turboImg,
+        badge: 'Akció',
+        oldPrice: 250000,
+        price: 225000,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 4,
+        name: 'Légszűrő',
+        brand: 'K&N',
+        image: legszuroImg,
+        badge: 'Akció',
+        oldPrice: 35000,
+        price: 28500,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 5,
+        name: 'Led fényszóró BMW E46',
+        brand: 'Osram',
+        image: ledizzoImg,
+        badge: 'Új',
+        oldPrice: 32000,
+        price: 30000,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 6,
+        name: 'Kipufogóvég',
+        brand: 'RaceCarbon',
+        image: kipuvegImg,
+        badge: 'Akció',
+        oldPrice: 15000,
+        price: 1000,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 7,
+        name: 'Sportkesztyű',
+        brand: 'Racing',
+        image: kesztyuImg,
+        badge: 'Akció',
+        oldPrice: 5000,
+        price: 4500,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 8,
+        name: '3 küllős kormány',
+        brand: 'Momo',
+        image: kormanyImg,
+        badge: 'Akció',
+        oldPrice: 95000,
+        price: 82000,
+        stockText: 'Raktáron'
+    },
+    {
+        id: 9,
+        name: 'Rendszámtáblatartó',
+        brand: 'HMN',
+        image: rendszamImg,
+        badge: 'Akció',
+        oldPrice: 6900,
+        price: 6700,
+        stockText: 'Raktáron'
+    },
+]
