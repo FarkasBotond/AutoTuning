cccf80d98c18e812ebcb5b326a8341d05ad7b470
FarkasBotond <fabozo2006@gmail.com>
2026-04-25 19:32:59 +0200

    searchbar style change

--- frontend/src/components/ui/SearchBar.vue
+++ frontend/src/components/ui/SearchBar.vue
@@ -23,9 +23,9 @@ const handleSearch = () => {
     <form @submit.prevent="handleSearch" class="w-full">
         <div class="relative w-full">
 
-
             <input v-model="search" type="text" placeholder="Termék keresése..."
-                class="h-14 w-full rounded-2xl border border-zinc-400 bg-zinc-100 pl-12 pr-4 text-base text-zinc-700 outline-none transition placeholder:text-zinc-400 focus:border-zinc-300 focus:bg-white">
+                class="h-14 w-full rounded-2xl border border-zinc-300 bg-zinc-100 pl-12 pr-4 text-base text-zinc-800 shadow-sm outline-none transition duration-200 placeholder:text-zinc-500 focus:border-red-500 focus:bg-white focus:shadow-[0_0_0_4px_rgba(239,68,68,0.15)]" />
+
         </div>
     </form>
 </template>
