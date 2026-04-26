c7dd27715ffb826927f642fa9a0714493d829759
Gaboao <pogabor3@gmail.com>
2026-04-26 16:57:23 +0200

    DatabaseSeeder

--- backend/database/seeders/DatabaseSeeder.php
+++ backend/database/seeders/DatabaseSeeder.php
@@ -14,7 +14,8 @@ class DatabaseSeeder extends Seeder
     public function run(): void
     {
         $this->call([
-            CarDataSeeder::class,
+            CarBrandSeeder::class,
+            CarModelSeeder::class
         ]);
 
         User::factory()->create([
