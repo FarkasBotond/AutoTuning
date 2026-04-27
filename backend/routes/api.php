aa4dbef7989b7b916933bd1ca9d704d9b493928f
Gaboao <pogabor3@gmail.com>
2026-04-27 15:20:26 +0200

    feat: add public order route

--- backend/routes/api.php
+++ backend/routes/api.php
@@ -5,44 +5,83 @@ use App\Http\Controllers\RegistrationController;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\CarBrandController;
 use App\Http\Controllers\CarModelController;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\TuningProductController;
 use App\Http\Controllers\TuningCompanyController;
 use App\Http\Controllers\ServiceCategoryController;
+use App\Http\Controllers\OrderController;
+use Illuminate\Support\Facades\Route;
 
 Route::post('/registration', [RegistrationController::class, 'registration'])->name('registration');
 
-Route::get('/users/{id}', [UserController::class, 'show'])->name('users.show')->whereNumber('id');
-
 Route::post('/login', [AuthController::class, 'authenticate'])->name('login');
 
+Route::get('/users/{id}', [UserController::class, 'show'])
+    ->name('users.show')
+    ->whereNumber('id');
+
 // Public read-only routes
-Route::get('/car-brands', [CarBrandController::class, 'index'])->name('car-brands.index');
-Route::get('/car-brands/{carBrand}', [CarBrandController::class, 'show'])->name('car-brands.show');
-Route::get('/car-models', [CarModelController::class, 'index'])->name('car-models.index');
-Route::get('/car-models/{carModel}', [CarModelController::class, 'show'])->name('car-models.show');
+Route::get('/car-brands', [CarBrandController::class, 'index'])
+    ->name('car-brands.index');
 
-Route::get('/tuning-products', [TuningProductController::class, 'index'])->name('tuning-products.index');
-Route::get('/tuning-products/{tuningProduct}', [TuningProductController::class, 'show'])->name('tuning-products.show')->whereNumber('tuningProduct');
+Route::get('/car-brands/{carBrand}', [CarBrandController::class, 'show'])
+    ->name('car-brands.show');
 
-Route::get('/tuning-companies', [TuningCompanyController::class, 'index'])->name('tuning-companies.index');
-Route::get('/tuning-companies/{tuningCompany}', [TuningCompanyController::class, 'show'])->name('tuning-companies.show')->whereNumber('tuningCompany');
+Route::get('/car-models', [CarModelController::class, 'index'])
+    ->name('car-models.index');
 
-Route::get('/service-categories', [ServiceCategoryController::class, 'index'])->name('service-categories.index');
-// Protected routes
+Route::get('/car-models/{carModel}', [CarModelController::class, 'show'])
+    ->name('car-models.show');
+
+Route::get('/tuning-products', [TuningProductController::class, 'index'])
+    ->name('tuning-products.index');
+
+Route::get('/tuning-products/{tuningProduct}', [TuningProductController::class, 'show'])
+    ->name('tuning-products.show')
+    ->whereNumber('tuningProduct');
+
+Route::get('/tuning-companies', [TuningCompanyController::class, 'index'])
+    ->name('tuning-companies.index');
+
+Route::get('/tuning-companies/{tuningCompany}', [TuningCompanyController::class, 'show'])
+    ->name('tuning-companies.show')
+    ->whereNumber('tuningCompany');
+
+Route::get('/service-categories', [ServiceCategoryController::class, 'index'])
+    ->name('service-categories.index');
+
+// Public order route - vendégként is működik
+Route::post('/orders', [OrderController::class, 'store'])
+    ->name('orders.store');
+
+// Protected user routes
 Route::middleware('auth:sanctum')->group(function () {
-    Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-    Route::put('/profile/email', [UserController::class, 'updateEmail'])->name('profile.email.update');
-    Route::put('/profile/password', [UserController::class, 'updatePassword'])->name('profile.password.update');
+    Route::post('/logout', [AuthController::class, 'logout'])
+        ->name('logout');
+
+    Route::put('/profile/email', [UserController::class, 'updateEmail'])
+        ->name('profile.email.update');
+
+    Route::put('/profile/password', [UserController::class, 'updatePassword'])
+        ->name('profile.password.update');
 });
 
 // Admin routes
 Route::middleware(['auth:sanctum', 'admin'])->group(function () {
-    Route::post('/car-brands', [CarBrandController::class, 'store'])->name('car-brands.store');
-    Route::put('/car-brands/{carBrand}', [CarBrandController::class, 'update'])->name('car-brands.update');
-    Route::delete('/car-brands/{carBrand}', [CarBrandController::class, 'destroy'])->name('car-brands.destroy');
-    Route::post('/car-models', [CarModelController::class, 'store'])->name('car-models.store');
-    Route::put('/car-models/{carModel}', [CarModelController::class, 'update'])->name('car-models.update');
-    Route::delete('/car-models/{carModel}', [CarModelController::class, 'destroy'])->name('car-models.destroy');
+    Route::post('/car-brands', [CarBrandController::class, 'store'])
+        ->name('car-brands.store');
+
+    Route::put('/car-brands/{carBrand}', [CarBrandController::class, 'update'])
+        ->name('car-brands.update');
+
+    Route::delete('/car-brands/{carBrand}', [CarBrandController::class, 'destroy'])
+        ->name('car-brands.destroy');
+
+    Route::post('/car-models', [CarModelController::class, 'store'])
+        ->name('car-models.store');
+
+    Route::put('/car-models/{carModel}', [CarModelController::class, 'update'])
+        ->name('car-models.update');
+
+    Route::delete('/car-models/{carModel}', [CarModelController::class, 'destroy'])
+        ->name('car-models.destroy');
 });
